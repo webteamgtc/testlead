5fe4b588366fe0818c307844350f8dc067751d2c
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-07-04 16:08:12 +0400

    dsasd

--- src/app/ib/components/growIBsection.jsx
+++ src/app/ib/components/growIBsection.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const GrowIBSection = () => {
     return (
-        <section className="bg-gradient-to-r from-[#293794] to-[#000021] text-[#E5E5EA] py-12 md:py-20 relative overflow-hidden text-center">
+        <section className="bg-gradient-to-r from-[#293794] to-[#000021] text-[#E5E5EA] py-8 md:py-20 relative overflow-hidden text-center">
                     <div className="absolute top-0 h-full w-full opacity-50 pointer-events-none">
                   <Image
                     src="/ib/pattern.webp"
@@ -13,7 +13,7 @@ const GrowIBSection = () => {
                   />
                 </div>
                    <div className="relative container mx-auto">
-                <div className="max-w-4xl mx-auto z-10 relative flex flex-col gap-5">
+                <div className="max-w-4xl mx-auto z-10 relative flex flex-col gap-3 md:gap-5">
                     <h4 className="text-lg md:text-2xl xl:text-3xl font-[200]">
                         Start Earning More.
                     </h4>
@@ -26,7 +26,7 @@ const GrowIBSection = () => {
                     <p className="text-xl xl:text-2xl text-white font-semibold">
                         <span className="text-white">Join us today,</span> and your traders will thank you tomorrow.
                     </p>
-                    <div className="flex flex-row justify-center items-center gap-4">
+                    <div className="flex flex-row justify-center items-center gap-4 mt-1">
                           <button className="bg-gradient-to-r  from-[#E1CFBB] to-[#956D42] text-sm md:text-base xl:text-lg text-white  px-6 py-3 rounded-lg transition-all duration-300">
                         Make the Switch
                     </button>
