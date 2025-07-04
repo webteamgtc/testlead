bd9ee5e4eef19cafc2badf94126d4e7d0d5f8856
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-07-04 18:59:11 +0400

    dsfdsdfs

--- src/app/partner-old/page.jsx
+++ src/app/partner-old/page.jsx
@@ -1,32 +1,8 @@
-'use client'
-import React, {useState} from 'react'
-import Header from '../ib/components/header';
-import Image from 'next/image';
-import CustomButton from '../components/common/CustomButton';
-import ComparisonTable from '../ib/components/table';
-import Paymore from '../ib/components/payMore';
-import Broker from '../ib/components/broker';
-import IBTestimonials from '../ib/components/IBTraders';
-import GrowIBSection from '../ib/components/growIBsection';
-import Footer from '../components/Footer';
-import Banner from './Component/Banner';
-
+import React from 'react'
 
 const page = () => {
-       const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <>
-    <Header />
-    <Banner setIsOpen={setIsOpen} />
-    <ComparisonTable setIsOpen={setIsOpen} />
-    <Paymore />
-    <Broker />
-    <IBTestimonials />
-    <GrowIBSection />
-    <Footer />
-    
-    </>
+    <div>page</div>
   )
 }
 
