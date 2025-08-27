1fe90af501b03d2d907582082cf3cceeb0da9ebc
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-08-27 10:28:16 +0400

    modna

--- src/app/[locale]/ng/switch-to-gtc/page.jsx
+++ src/app/[locale]/ng/switch-to-gtc/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react";
-import CommonPage from '../pages/switch-to-gtc'
+import CommonPage from '../../pages/switch-to-gtc'
 import { useTranslations } from "next-intl";
 import Meta from "@/app/components/common/MetaData";
 
