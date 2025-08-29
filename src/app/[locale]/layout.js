ff956f6b84677acd7956b4032fb5921ac3e11330
adeel <adeelcomsats070@gmail.com>
2025-08-29 15:02:18 +0500

    changes gtm

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,3 +1,5 @@
+
+import Script from "next/script";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import CookieConsent from "./components/CookieConsent";
@@ -5,16 +7,17 @@ import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 
-export default async function LocaleLayout({ children, params }) {
-  const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
 
-  const isArabicOrKurdish = locale === 'ar' || locale === 'ku';
+export default function RootLayout({
+  children,
+  params,
+}) {
+  const locale = params?.locale ?? "en";
+  const isArabicOrKurdish = locale === "ar" || locale === "ku";
+  const GTM_ID = "GTM-PSWH9QF";
 
   return (
-    <html lang={locale} dir={isArabicOrKurdish ? 'rtl' : 'ltr'}>
+    <html lang={locale} dir={isArabicOrKurdish ? "rtl" : "ltr"}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -26,24 +29,34 @@ export default async function LocaleLayout({ children, params }) {
           href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PSWH9QF');`,
-          }}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
         <style>{`
-          body {
-            font-family: '${isArabicOrKurdish ? 'Noto Kufi Arabic' : 'Poppins'}', sans-serif;
-          }
+          body { font-family: '${isArabicOrKurdish ? "Noto Kufi Arabic" : "Poppins"}', sans-serif; }
         `}</style>
       </head>
+
       <body>
+        {/* Google Tag Manager (noscript) – MUST be first in body */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+
+        {/* Google Tag Manager – load after hydration */}
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+
+        {/* your app */}
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <ToastContainer autoClose={3000} />
         <CookieConsent />
