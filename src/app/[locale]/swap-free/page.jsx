f98d04fbe69e71faba362c6c5dbc3bd655696bfc
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-10-01 11:55:58 +0400

    s

--- src/app/[locale]/swap-free/page.jsx
+++ src/app/[locale]/swap-free/page.jsx
@@ -36,7 +36,7 @@ const SwapFreePage = () => {
                 zapierUrl="https://hooks.zapier.com/hooks/catch/16420445/uh462x6/"
                 successPath="/swap-free/success"
             />
-        </>
+        </> 
     )
 }
  
